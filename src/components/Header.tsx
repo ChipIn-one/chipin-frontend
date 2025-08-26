46e7a75894e2d47617e7cd34ffcc94be71247304
syllik <syllik@gmail.com>
2025-08-26 15:34:39 +0400

    optimize logo and pwa config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,28 @@ import { LucideLogIn, LucideUserRoundPlus } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { Avatar, Box, Button, Card, Flex, IconButton, Text } from '@radix-ui/themes';
+import { Avatar, Box, Button, Flex, IconButton, Text } from '@radix-ui/themes';
 
 import { PROJECT_NAME } from 'constants/chipin';
 import { ROUTES } from 'constants/routes';
 import { useAuthStore } from 'store/authStore';
 
-import favicon from 'assets/favicon.png';
+import Logotype from 'assets/logo.svg?react';
 
 import AuthModal from './Modal/AuthModal';
 
 const StickyBox = styled(Box)`
-    background-color: var(--accent-2);
+    background-color: hsl(var(--accent-2) / 0.6);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
     position: sticky;
     top: 0;
     width: 100%;
+`;
+
+const StyledLogotype = styled(Logotype)`
+    width: 60px;
+    height: 60px;
 `;
 
 const Header = () => {
@@ -24,46 +31,44 @@ const Header = () => {
 
     return (
         <StickyBox>
-            <Card>
-                <Flex justify="between" align="center" p="4">
-                    <Flex gap="4" align="center">
-                        <Link to={isLoggedIn ? ROUTES.BALANCES : ROUTES.HOME}>
-                            <Avatar src={favicon} fallback="C" radius="full" />
-                        </Link>
-                        <Text size="6" weight="bold">
-                            {PROJECT_NAME}
-                        </Text>
-                    </Flex>
-                    <Flex gap="4" align="center">
-                        {isLoggedIn ? (
-                            <Flex gap="4" align="center">
-                                <Box display={{ initial: 'block', md: 'none' }}>
-                                    <IconButton variant="ghost">
-                                        <LucideUserRoundPlus />
-                                    </IconButton>
-                                </Box>
-                                <Box display={{ initial: 'none', md: 'block' }}>
-                                    <Avatar
-                                        size="3"
-                                        src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                                        radius="full"
-                                        fallback="A"
-                                    />
-                                </Box>
-                            </Flex>
-                        ) : (
-                            <AuthModal
-                                triggerElement={
-                                    <Button size="3" variant="outline">
-                                        Get started
-                                        <LucideLogIn />
-                                    </Button>
-                                }
-                            />
-                        )}
-                    </Flex>
+            <Flex justify="between" align="center" p="4">
+                <Flex gap="4" align="center" justify="center">
+                    <Link to={isLoggedIn ? ROUTES.BALANCES : ROUTES.HOME}>
+                        <StyledLogotype />
+                    </Link>
+                    <Text size="6" weight="bold">
+                        {PROJECT_NAME}
+                    </Text>
                 </Flex>
-            </Card>
+                <Flex gap="4" align="center">
+                    {isLoggedIn ? (
+                        <Flex gap="4" align="center">
+                            <Box display={{ initial: 'block', md: 'none' }}>
+                                <IconButton variant="ghost">
+                                    <LucideUserRoundPlus />
+                                </IconButton>
+                            </Box>
+                            <Box display={{ initial: 'none', md: 'block' }}>
+                                <Avatar
+                                    size="3"
+                                    src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+                                    radius="full"
+                                    fallback="A"
+                                />
+                            </Box>
+                        </Flex>
+                    ) : (
+                        <AuthModal
+                            triggerElement={
+                                <Button size="3" variant="outline">
+                                    Get started
+                                    <LucideLogIn />
+                                </Button>
+                            }
+                        />
+                    )}
+                </Flex>
+            </Flex>
         </StickyBox>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import react from '@vitejs/plugin-react-swc';
@@ -8,9 +9,11 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     plugins: [
+        svgr(),
         react(),
         tsconfigPaths(),
         VitePWA({
+            //TODO: ADD DEV/PROD ENV
             registerType: 'autoUpdate',
             injectRegister: false,
 
@@ -20,16 +23,18 @@ export default defineConfig({
             },
 
             manifest: {
-                name: 'vite-project',
-                short_name: 'vite-project',
-                description: 'vite-project',
+                name: 'ChipIn',
+                short_name: 'ChipIn',
+                description: 'Share expenses without stress',
                 theme_color: '#ffffff',
+                display: 'standalone',
             },
 
             workbox: {
                 globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
                 cleanupOutdatedCaches: true,
                 clientsClaim: true,
+                skipWaiting: true,
             },
 
             devOptions: {
