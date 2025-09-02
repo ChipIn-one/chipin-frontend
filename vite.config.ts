d2ca596c93809620f97e9469bd42c6ec44d84cf2
syllik <syllik@gmail.com>
2025-09-02 19:11:51 +0400

    update: configs

--- vite.config.ts
+++ vite.config.ts
@@ -24,38 +24,38 @@ export default defineConfig({
                 display: 'standalone',
                 icons: [
                     {
-                        src: '/assets/pwa-64x64.png',
+                        src: '/pwa-64x64.png',
                         sizes: '64x64',
                         type: 'image/png',
                     },
                     {
-                        src: '/assets/apple-touch-icon-180x180.png',
+                        src: '/apple-touch-icon-180x180.png',
                         sizes: '180x180',
                         type: 'image/png',
                     },
                     {
-                        src: '/assets/pwa-192x192.png',
+                        src: '/pwa-192x192.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: '/assets/pwa-512x512.png',
+                        src: '/pwa-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
                     },
                     {
-                        src: '/assets/maskable-icon-512x512.png',
+                        src: '/maskable-icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'maskable',
                     },
                     {
-                        src: '/assets/favicon.ico',
+                        src: '/favicon.ico',
                         sizes: '48x48',
                         type: 'image/x-icon',
                     },
                     {
-                        src: '/assets/favicon.svg',
+                        src: '/favicon.svg',
                         sizes: 'any',
                         type: 'image/svg+xml',
                     },
