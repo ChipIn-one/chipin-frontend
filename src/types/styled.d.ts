d881c0ab02bcbd8ca9b419a79def67872847eb1e
syllik <syllik@gmail.com>
2026-07-05 17:35:43 +0700

    fix: cards filtrations and styles

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,19 +1,7 @@
 import 'styled-components';
 
 type RadixColorScale = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
-type RadixAlphaScale =
-    | 'a1'
-    | 'a2'
-    | 'a3'
-    | 'a4'
-    | 'a5'
-    | 'a6'
-    | 'a7'
-    | 'a8'
-    | 'a9'
-    | 'a10'
-    | 'a11'
-    | 'a12';
+type RadixAlphaScale = `A${RadixColorScale}`;
 type RadixPaletteName =
     | 'amber'
     | 'blue'
